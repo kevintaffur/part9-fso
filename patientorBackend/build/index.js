524b7aa8a9b08ee3acb4828a0a8322094ef7fc40
Kevin <86487929+tar83@users.noreply.github.com>
2022-11-17 14:00:24 -0500

    exercise 9.10

--- patientorBackend/build/index.js
+++ patientorBackend/build/index.js
@@ -4,12 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const diagnoses_1 = __importDefault(require("./routes/diagnoses"));
+const PORT = 3001;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const PORT = 3000;
+app.use(cors_1.default);
 app.get("/api/ping", (_req, res) => {
     res.send("pong");
 });
+app.use("/api/diagnoses", diagnoses_1.default);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- patientorBackend/src/index.ts
+++ patientorBackend/src/index.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import cors from "cors";
+import diagnosisRouter from "./routes/diagnoses";
+const PORT = 3001;
 
 const app = express();
 app.use(express.json());
-app.use(cors);
-const PORT = 3001;
+app.use(cors());
 
 app.get("/api/ping", (_req, res) => {
+  console.log("pong");
+
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnosisRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
